2e56068c0a322fd9014290a782e4056d431281d7
Sebastian-Saez <sebastian.saez@uaysen.cl>
2025-05-06 19:09:09 -0300

    frontend v1.6.3: Aplicación móvil ColmedAysen - actualización archivo de configuración iOS

---
+++ frontend-colmed/frontend-colmed-coy/src-capacitor/capacitor.config.ts
@@ -0,0 +1,22 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+import dotenv from 'dotenv';
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
+
+
+const config: CapacitorConfig = {
+  appId: 'org.colmed.aysen.app',
+  appName: 'ColmedAysén',
+  webDir: 'www',
+  ios: {
+    buildFlag: (process.env.IOS_BUILD_FLAGS || '').split(' ')
+  },
+  // server: {
+  //   hostname: 'app.colmedaysen.cl',    
+  //   androidScheme: 'https',
+  //   iosScheme: 'https'
+  // }
+};
+
+export default config;
