6e07f12bc3af295d9236b17f8101e873c1c1c1c5
Sebastian-Saez <sebastian.saez@uaysen.cl>
2025-05-08 14:31:09 -0300

    frontend v1.6.4: Aplicación móvil ColmedAysen - actualización capacitor.config.ts - se quitan logs para AppFlow

--- frontend-colmed/frontend-colmed-coy/src-capacitor/capacitor.config.ts
+++ frontend-colmed/frontend-colmed-coy/src-capacitor/capacitor.config.ts
@@ -4,16 +4,6 @@ import type { CapacitorConfig } from '@capacitor/cli';
 // Cargar las variables de entorno desde el archivo .env
 // dotenv.config();
 
-
-// LOGS PARA DEBUG EN APPFLOW
-console.log('[CAPACITOR_CONFIG_TS] Inicio de capacitor.config.ts');
-const rawBuildFlags = process.env.IOS_BUILD_FLAGS;
-console.log(`[CAPACITOR_CONFIG_TS] Valor RAW de process.env.IOS_BUILD_FLAGS: "${rawBuildFlags}"`);
-
-const buildFlagsArray = (rawBuildFlags || '').split(' ');
-console.log('[CAPACITOR_CONFIG_TS] Array buildFlag procesado:', buildFlagsArray);
-// FIN DE LOGS PARA DEBUG
-
 const config: CapacitorConfig = {
   appId: 'org.colmed.aysen.app',
   appName: 'ColmedAysén',
